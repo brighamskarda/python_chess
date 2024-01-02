cd85e36cc1413a0696e73f86eb5fa4b530771366
Brigham Skarda <brighamskarda@gmail.com>
2024-01-01 18:38:32 -0700

    Add first step of tick logic

--- mlchess.py
+++ mlchess.py
@@ -1,9 +1,5 @@
 import chess
-import math
-
-
-        
-
+import copy
 
 class ChessNode:
     '''!
@@ -11,12 +7,12 @@ class ChessNode:
     upon instantiation. (Its children can just be an empty list.)
     '''
     __center_pieces = [chess.D4, chess.E4, chess.D5, chess.E5]
-    def __init__(self, board: chess.Board, parent: 'ChessNode', children: ['ChessNode']):
+    def __init__(self, board: chess.Board, parent: 'ChessNode'):
         self.board = board
         self.score = ChessNode.__score_board(board)
         self.takes_piece = ChessNode.__takes_piece(board)
         self.parent = parent
-        self.children = children
+        self.children: list[ChessNode] = []
         
     
     @staticmethod
@@ -77,6 +73,8 @@ class ChessNode:
     
     @staticmethod
     def __takes_piece(board: chess.Board) -> bool:
+        if len(board.move_stack) == 0:
+            return False
         move = board.pop()
         if board.piece_at(move.to_square) != None:
             board.push(move)
@@ -85,15 +83,102 @@ class ChessNode:
             chess.square_file(move.from_square) != chess.square_file(move.to_square)):
             board.push(move)
             return True
+        board.push(move)
         return False
         
 
 class MLChess:
+    '''!
+    This class will find the best move for the computer to make, and then make it. It also accepts
+    user input for moves.
+    '''
     CHECK_DEPTH = 3
     NUM_BEST = 5
     NUM_LEAST_DEPTH = 5
     def __init__(self, board: chess.Board = chess.Board()):
-        self.root = ChessNode(board, None, [])
+        self.root = ChessNode(board, None)
+        
+    def user_move(self, move: str) -> bool:
+        '''!
+        @return True if the move was valid and successful. Otherwise false.
+        '''
+        new_board = self.root.board.copy()
+        try:
+            new_board.push_uci(move)
+        except:
+            return False
+        self.root = ChessNode(new_board, None)
+        return True
     
-    def __get_average_leaf_node_score(node: ChessNode):
+    def computer_move_tick(self, ticks: int = 3) -> chess.Move:
+        '''!
+        The computer makes a move, and returns the move in case you want to see what it was.
+        '''
+        if self.root.board.is_checkmate():
+            print('No possible moves')
+            return
+        self.__initial_scoring()
+        for i in range(ticks):
+            print(f'Working on tick {i+1}/{ticks}')
+            # Evaluate moves that put king in check
+            leaf_nodes = MLChess.__get_all_leaf_nodes(self.root)
+            for leaf in leaf_nodes:
+                if leaf.board.is_check():
+                    MLChess.__king_check_moves(leaf, MLChess.CHECK_DEPTH - 1)
+        
+        # Find and return best move
+        best_position = self.root.children[0]
+        for child in self.root.children:
+            if child.score > best_position.score:
+                best_position = child
+        self.root = ChessNode(best_position.board, None)
+        
+        return self.root.board.peek()
     
+    def __initial_scoring(self):
+        for m in self.root.board.legal_moves:
+            new_board = self.root.board.copy()
+            new_board.push(m)
+            new_node = ChessNode(new_board, self.root)
+            self.root.children.append(new_node)
+    
+    @staticmethod
+    def __king_check_moves(node: ChessNode, depth: int):
+        if depth == 0:
+            return
+        # Make all possible opponent moves
+        for m in node.board.legal_moves:
+            new_board = node.board.copy()
+            new_board.push(m)
+            new_node = ChessNode(new_board, node)
+            node.children.append(new_node)
+            # Make all moves that put opponent in check again
+            for m2 in new_board.legal_moves:
+                new_board2 = new_board.copy()
+                new_board2.push(m2)
+                if new_board2.is_check():
+                    new_node2 = ChessNode(new_board2, new_node)
+                    new_node.children.append(new_node2)
+                    MLChess.__king_check_moves(new_node2, depth - 1)
+        
+        
+    
+    @staticmethod
+    def __get_all_leaf_nodes(node: ChessNode) -> list[ChessNode]:
+        if len(node.children) == 0:
+            return [node]
+        return_list = []
+        for n in node.children:
+            for n2 in MLChess.__get_all_leaf_nodes(n):
+                return_list.append(n2)
+        return return_list
+        
+        
+    @staticmethod
+    def __get_average_leaf_node_score(node: ChessNode) -> int:
+        if len(node.children) == 0:
+            return node.score
+        total_score = 0
+        for n in node.children:
+            total_score += MLChess.__get_average_leaf_node_score(n)
+        return total_score / len(node.children)
--- pychess.py
+++ pychess.py
@@ -1,11 +1,24 @@
-from mlchess import ChessNode
+from mlchess import MLChess
 import timeit
 import chess
 
 # myChess = MLChess()
-stp = '''
-from mlchess import ChessNode
-import chess
-board = chess.Board('rn2kbn1/pppqppp1/3p4/7r/4P2p/2N5/PPPP1PPP/R1BQKB1R')
-'''
-print(timeit.timeit(stmt='ChessNode._ChessNode__score_board(board)', setup=stp,number=100_000)/100_000)
+# stp = '''
+# from mlchess import ChessNode
+# import chess
+# board = chess.Board('rn2kbn1/pppqppp1/3p4/7r/4P2p/2N5/PPPP1PPP/R1BQKB1R')
+# '''
+# print(timeit.timeit(stmt='ChessNode._ChessNode__score_board(board)', setup=stp,number=100_000)/100_000)
+
+board = MLChess()
+while True:
+    board.computer_move_tick()
+    print(board.root.board)
+    input()
+# while not board.root.board.is_checkmate():
+#     print(board.root.board)
+#     inp = input('Enter a move:')
+#     if not board.user_move(inp):
+#         print('Invalid move!')
+    
+    
